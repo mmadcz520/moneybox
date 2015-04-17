8697ba1d3e196e074d069aa198b57414dc463df7
zhoulongfei <mmadcz520@gmail.com>
2015-04-17 16:57:35 +0800

    ProductFragment  update

---
+++ app/src/main/java/com/changtou/moneybox/module/page/ConfirmActivity.java
@@ -0,0 +1,24 @@
+package com.changtou.moneybox.module.page;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.changtou.R;
+
+/**
+ * 描述: 投资确认页
+ *
+ * @author zhoulongfei
+ * @since 2015-04-17
+ */
+public class ConfirmActivity extends CTBaseActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm);
+    }
+}
--- app/src/main/java/com/changtou/moneybox/module/page/ProductDetailsActivity.java
+++ app/src/main/java/com/changtou/moneybox/module/page/ProductDetailsActivity.java
@@ -1,7 +1,11 @@
 package com.changtou.moneybox.module.page;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 import com.changtou.R;
@@ -19,14 +23,25 @@ public class ProductDetailsActivity extends CTBaseActivity
     RelativeLayout.LayoutParams parentParams;
     RelativeLayout.LayoutParams paneBomParams;
 
+
     /**
      * @see CTBaseActivity#initView(Bundle)
      */
     protected void initView(Bundle bundle)
     {
         setContentView(R.layout.product_details);
-
         final VerticalPager vp = (VerticalPager)findViewById(R.id.vertyPager1);
+
+        Button button = (Button)findViewById(R.id.confirm_button);
+
+        final Intent intent = new Intent(this, ConfirmActivity.class);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                startActivity(intent);
+            }
+        });
 
     }
 }
