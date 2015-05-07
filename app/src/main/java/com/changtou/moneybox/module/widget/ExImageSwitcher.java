19f28a407a9c3a900f17010d0000da7e0c1a7b3c
zhoulongfei <mmadcz520@gmail.com>
2015-05-07 14:14:35 +0800

    banner 实现方式的改变

--- app/src/main/java/com/changtou/moneybox/module/widget/ExImageSwitcher.java
+++ app/src/main/java/com/changtou/moneybox/module/widget/ExImageSwitcher.java
@@ -1,6 +1,9 @@
 package com.changtou.moneybox.module.widget;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+
 import android.os.AsyncTask;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -17,22 +20,22 @@ import android.widget.ViewSwitcher.ViewFactory;
 
 import com.changtou.R;
 import com.changtou.moneybox.common.utils.AppUtil;
+import com.changtou.moneybox.common.utils.AsyncImageLoader;
+
+import java.util.ArrayList;
 
 /**
  * 自定义图片滑动组件
  * 
  * @author Jone
  */
-public class ExImageSwitcher extends FrameLayout implements OnTouchListener,
+public class ExImageSwitcher extends FrameLayout implements OnTouchListener,AsyncImageLoader.ImageCallback,
 		ViewFactory {
 	/**
 	 * ImagaSwitcher 的引用
 	 */
 	private ImageSwitcher mImageSwitcher;
-	/**
-	 * 图片id数组
-	 */
-	private int[] imgIds;
+
 	/**
 	 * 当前选中的图片id序号
 	 */
@@ -52,8 +55,12 @@ public class ExImageSwitcher extends FrameLayout implements OnTouchListener,
 
 	private Context mContext = null;
 
+	private int bannerCnt = 0;
+
 	private int mSFlag = 1;
 	private int mSCnt = 0;
+
+	private ArrayList<Bitmap> mBannerImage = null;
 
 	public ExImageSwitcher(Context context) {
 		this(context, null);
@@ -78,12 +85,13 @@ public class ExImageSwitcher extends FrameLayout implements OnTouchListener,
 	 * 设置显示的图片
 	 */
 	public void setImage(int[] res) {
-		imgIds = res;
-
+		bannerCnt = res.length;
 		linearLayout = (LinearLayout) findViewById(R.id.viewGroup);
 
-		tips = new ImageView[imgIds.length];
-		for (int i = 0; i < imgIds.length; i++) {
+		mBannerImage = new ArrayList<>();
+
+		tips = new ImageView[bannerCnt];
+		for (int i = 0; i < bannerCnt; i++) {
 			ImageView mImageView = new ImageView(mContext);
 			tips[i] = mImageView;
 			LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
@@ -94,27 +102,60 @@ public class ExImageSwitcher extends FrameLayout implements OnTouchListener,
 			layoutParams.width = AppUtil.dip2px(mContext, 7);
 			layoutParams.height = AppUtil.dip2px(mContext, 7);
 
-			mImageView.setBackgroundResource(R.drawable.page_indicator_unfocused);
+//			mImageView.setBackgroundResource(R.drawable.page_indicator_unfocused);
 			linearLayout.addView(mImageView, layoutParams);
 		}
 
-		// 这个我是从上一个界面传过来的，上一个界面是一个GridView
-		mImageSwitcher.setImageResource(imgIds[currentPosition]);
+		mImageSwitcher.setImageResource(res[currentPosition]);
+		setImageBackground(currentPosition);
+	}
+
+	/**
+	 * 设置显示的图片
+	 */
+	public void setImage(String[] url) {
+		linearLayout = (LinearLayout) findViewById(R.id.viewGroup);
+		tips = new ImageView[url.length];
+
+		mBannerImage = new ArrayList<>();
+
+		for (int i = 0; i < url.length; i++) {
+			ImageView mImageView = new ImageView(mContext);
+			tips[i] = mImageView;
+			LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+					new ViewGroup.LayoutParams(LayoutParams.WRAP_CONTENT,
+							LayoutParams.WRAP_CONTENT));
+			layoutParams.rightMargin = AppUtil.dip2px(mContext, 4);
+			layoutParams.leftMargin = AppUtil.dip2px(mContext, 4);
+			layoutParams.width = AppUtil.dip2px(mContext, 7);
+			layoutParams.height = AppUtil.dip2px(mContext, 7);
+
+//			mImageView.setBackgroundResource(R.drawable.page_indicator_unfocused);
+			linearLayout.addView(mImageView, layoutParams);
+
+			AsyncImageLoader asyncImageLoader = new AsyncImageLoader();
+			asyncImageLoader.loadImageAsyn("http://appt.changtounet.com/Img/home_default_banner.png", this);
+		}
 
 		setImageBackground(currentPosition);
 	}
 
 	/**
 	 * 设置选中的tip的背景
-	 * 
+	 *
 	 * @param selectItems
 	 */
-	private void setImageBackground(int selectItems) {
-		for (int i = 0; i < tips.length; i++) {
-			if (i == selectItems) {
-				tips[i].setBackgroundResource(R.drawable.page_indicator_focused);
-			} else {
-				tips[i].setBackgroundResource(R.drawable.page_indicator_unfocused);
+	private void setImageBackground(int selectItems)
+	{
+		for (int i = 0; i < tips.length; i++)
+		{
+			if (i == selectItems)
+			{
+//				tips[i].setBackgroundResource(R.drawable.page_indicator_focused);
+			}
+			else
+			{
+//				tips[i].setBackgroundResource(R.drawable.page_indicator_unfocused);
 			}
 		}
 	}
@@ -124,62 +165,92 @@ public class ExImageSwitcher extends FrameLayout implements OnTouchListener,
 	 * 
 	 * @see android.widget.ViewSwitcher.ViewFactory#makeView()
 	 */
-	public View makeView() {
+	public View makeView()
+	{
 		final ImageView i = new ImageView(mContext);
-		i.setBackgroundColor(0xff000000);
+		i.setBackgroundColor(0x5e5e5e);
 		i.setScaleType(ImageView.ScaleType.CENTER_CROP);
-		i.setLayoutParams(new LayoutParams(
-				LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+		i.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
 		return i;
 	}
 
-	public boolean onTouch(View v, MotionEvent event) {
-		switch (event.getAction()) {
-		case MotionEvent.ACTION_DOWN: {
-			// 手指按下的X坐标
-			downX = event.getX();
-			mSCnt = 0;
-			break;
-		}
-		case MotionEvent.ACTION_UP: {
-			float lastX = event.getX();
-			// 抬起的时候的X坐标大于按下的时候就显示上一张图片
-			if (lastX > downX) {
-				mSFlag = 0;
-				mImageSwitcher.setInAnimation(AnimationUtils.loadAnimation(
-						mContext, R.anim.switcher_left_in));
-				mImageSwitcher.setOutAnimation(AnimationUtils.loadAnimation(
-						mContext, R.anim.switcher_right_out));
-				if (currentPosition > 0) {
-					// 设置动画，这里的动画比较简单，不明白的去网上看看相关内容
-					currentPosition--;
-				} else {
-					currentPosition = imgIds.length - 1;
+	public boolean onTouch(View v, MotionEvent event)
+	{
+		switch (event.getAction())
+		{
+			case MotionEvent.ACTION_DOWN:
+			{
+				// 手指按下的X坐标
+				downX = event.getX();
+				mSCnt = 0;
+				break;
+			}
+			case MotionEvent.ACTION_UP:
+			{
+				float lastX = event.getX();
+				// 抬起的时候的X坐标大于按下的时候就显示上一张图片
+				if (lastX > downX)
+				{
+					mSFlag = 0;
+					mImageSwitcher.setInAnimation(AnimationUtils.loadAnimation(
+							mContext, R.anim.switcher_left_in));
+					mImageSwitcher.setOutAnimation(AnimationUtils.loadAnimation(
+							mContext, R.anim.switcher_right_out));
+					if (currentPosition > 0)
+					{
+						// 设置动画，这里的动画比较简单，不明白的去网上看看相关内容
+						currentPosition--;
+					}
+					else
+					{
+						currentPosition = bannerCnt - 1;
+					}
+				}
+
+				if (lastX < downX)
+				{
+					mSFlag = 1;
+					mImageSwitcher.setInAnimation(AnimationUtils.loadAnimation(
+							mContext, R.anim.switcher_right_in));
+					mImageSwitcher.setOutAnimation(AnimationUtils.loadAnimation(
+							mContext, R.anim.switcher_left_out));
+					if (currentPosition < bannerCnt - 1)
+					{
+						currentPosition++;
+					}
+					else
+					{
+						currentPosition = 0;
+					}
+
+					setImage();
 				}
 			}
-
-			if (lastX < downX) {
-				mSFlag = 1;
-				mImageSwitcher.setInAnimation(AnimationUtils.loadAnimation(
-						mContext, R.anim.switcher_right_in));
-				mImageSwitcher.setOutAnimation(AnimationUtils.loadAnimation(
-						mContext, R.anim.switcher_left_out));
-				if (currentPosition < imgIds.length - 1) {
-					currentPosition++;
-				} else {
-					currentPosition = 0;
-				}
-			}
-
-			mImageSwitcher.setImageResource(imgIds[currentPosition
-					% imgIds.length]);
-			setImageBackground(currentPosition);
-
-		}
 			break;
 		}
 
 		return true;
+	}
+
+	private void setImage()
+	{
+		ImageView imageView = (ImageView)mImageSwitcher.getNextView();
+		imageView.setImageBitmap(mBannerImage.get(currentPosition));
+		imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+		mImageSwitcher.showNext();
+		setImageBackground(currentPosition);
+	}
+
+	/**
+	 * 异步加载网络图片
+	 *
+	 * @param path
+	 * @param bitmap
+	 */
+	public void loadImage(String path, Bitmap bitmap)
+	{
+		mBannerImage.add(bitmap);
+		setImageBackground(currentPosition);
 	}
 
 	private class SwitchTask extends AsyncTask<String, Integer, String> {
@@ -199,22 +270,20 @@ public class ExImageSwitcher extends FrameLayout implements OnTouchListener,
 				if (currentPosition > 0) {
 					currentPosition--;
 				} else {
-					currentPosition = imgIds.length - 1;
+					currentPosition = bannerCnt - 1;
 				}
 			} else if (mSFlag == 1) {
 				mImageSwitcher.setInAnimation(AnimationUtils.loadAnimation(
 						mContext, R.anim.switcher_right_in));
 				mImageSwitcher.setOutAnimation(AnimationUtils.loadAnimation(
 						mContext, R.anim.switcher_left_out));
-				if (currentPosition < imgIds.length - 1) {
+				if (currentPosition < bannerCnt - 1) {
 					currentPosition++;
 				} else {
 					currentPosition = 0;
 				}
 			}
-			mImageSwitcher.setImageResource(imgIds[currentPosition
-					% imgIds.length]);
-			setImageBackground(currentPosition);
+			setImage();
 		}
 
 		/*
@@ -250,4 +319,15 @@ public class ExImageSwitcher extends FrameLayout implements OnTouchListener,
 			}
 		}
 	}
+
+	public static Bitmap zoomBitmap(Bitmap bitmap, int width, int height) {
+		         int w = bitmap.getWidth();
+		         int h = bitmap.getHeight();
+		         Matrix matrix = new Matrix();
+		         float scaleWidth = ((float) width / w);
+		         float scaleHeight = ((float) height / h);
+		         matrix.postScale(scaleWidth, scaleHeight);
+		       Bitmap newbmp = Bitmap.createBitmap(bitmap, 0, 0, w, h, matrix, true);
+		         return newbmp;
+		     }
 }
